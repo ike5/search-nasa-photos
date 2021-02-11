32ef1d012c2920c1f661989340a22508ecacd0cb
Ike Maldonado <maldonadoike@gmail.com>
2021-02-10 22:30:44 -0800

    fixed bug on state

--- dist/src.a2b27638.js
+++ dist/src.a2b27638.js
@@ -371,7 +371,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "42363" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "39927" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
